6145655262b49f88f824c83f1fb90870c3b56701
Johann Forster <johann.forster@htw-dresden.de>
2020-09-29 13:39:13 +0200

    Update minimal_sender

--- cmd/minimal_sender/main.go
+++ cmd/minimal_sender/main.go
@@ -5,11 +5,12 @@ import (
 	"time"
 
 	"github.com/Waziup/single_chan_pkt_fwd/SX127X"
+	"github.com/Waziup/single_chan_pkt_fwd/lora"
 )
 
 func main() {
 	log.SetFlags(0)
-	SX127X.LogLevel = SX127X.LogLevelDebug
+	// SX127X.LogLevel = SX127X.LogLevelDebug
 
 	radio, err := SX127X.Discover()
 	if err != nil {
@@ -17,6 +18,8 @@ func main() {
 	}
 
 	log.Printf("SX1272 init ...")
+
+	// must("SetIQInversion:", radio.SetIQInversion(true))
 
 	must("SetCR:", radio.SetCR(SX127X.CR_5))
 	must("SetBW:", radio.SetBW(SX127X.BW_125))
@@ -31,7 +34,17 @@ func main() {
 
 	msg := []byte{0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7}
 	start := time.Now()
-	must("Write:", radio.Write(msg))
+	must("Send:", radio.Send(&lora.TxPacket{
+		InvertPolar: true,
+		Modulation:  "LORA",
+		LoRaCR:      5,
+		LoRaBW:      0x08,
+		Freq:        868100000,
+		Datarate:    12,
+		Power:       14,
+		Data:        msg,
+	}))
+	// must("Write:", radio.Write(msg))
 	end := time.Now()
 	log.Printf("LoRa Sent in %s", end.Sub(start))
 }
